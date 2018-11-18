68950ffcde59ff81902797fd071e02e0a1efab1d
gustavovianaA <38870251+gustavovianaA@users.noreply.github.com>
2018-11-18 19:09:18 -0200

    layout change nav + larger content
    nav also changes the portfolio layout.
    container-fluid for a larger content layout.

--- js/base.js
+++ js/base.js
@@ -1,5 +1,71 @@
 $(document).ready(function(){
 
+//Mudar o layout no portfolio e depois voltar ao normal
+
+  function layout_change(){
+
+    
+    $("#sem_info").removeClass("col-lg-9");
+    $("#sem_info").removeClass("col-12");
+    $("#sem_info").addClass("col-11");
+
+  $("#my_info").removeClass("col-lg-3   justify-content-center pt-lg-0");
+  $("#my_info").addClass("justify-content-end    col-lg-10 pt-lg-4");
+  
+
+
+    $(".sobre_titulo").addClass("col-12 ");
+   
+      
+
+
+}
+
+function layout_return(){
+
+    $("#sem_info").addClass("col-lg-9");
+    $("#sem_info").removeClass("col-11");
+    $("#sem_info").addClass("col-12");
+
+  $("#my_info").removeClass(" col-lg-10 justify-content-end pt-lg-4");
+  $("#my_info").addClass("col-lg-3  mx-auto justify-content-center pt-lg-0");
+
+    $(".sobre_titulo").removeClass("col-12");
+    
+
+
+}
+
+$("#next").click(function(){
+
+if($(".atual").attr("id") == 2){
+
+    layout_change();
+}else{
+    layout_return();
+    }
+
+});
+
+$("#back").click(function(){
+
+layout_return();
+
+});
+
+$("#conhecimento").click(function(){
+layout_return();
+})
+$("#formacao").click(function(){
+layout_return();
+})
+$("#portfolio").click(function(){
+layout_change();
+})
+
+
+//----------------
+
 $(".site_conteudo").hide();
 
 setTimeout(function(){
